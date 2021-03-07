d71240c4b5716f4d6ef4ce6bda2e750109779f95
Derrick De Mel <derrick_demel@msn.com>
2021-03-07 15:06:19 +0000

    Ticket 1 Fixing Unit Tests

---
+++ PDR.PatientBooking.Service/Validation/EmailAddressValidation.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PDR.PatientBooking.Service.Validation
+{
+    public class EmailAddressValidation
+    {
+        public string Email { get; set; }
+
+        public bool IsValid()
+        {
+            return Regex.IsMatch(Email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$", RegexOptions.IgnoreCase);
+        }
+    }
+}
